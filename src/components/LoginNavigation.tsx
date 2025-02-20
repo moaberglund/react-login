f569ddcf6b989b9098be719d7871873c9c1be47c
moaberglund <moa.21@hotmail.com>
2025-02-20 12:13:11 +0100

    switch structure in routing to fit several layouts

--- src/components/LoginNavigation.tsx
+++ src/components/LoginNavigation.tsx
@@ -4,7 +4,7 @@ const LoginNavigation = () => {
   return (
     <nav>
         <ul>
-            <li><NavLink to="/">Login</NavLink></li>
+            <li><NavLink to="/login">Login</NavLink></li>
             <li><NavLink to="/signup">Sign up</NavLink></li>
         </ul>
     </nav>
--- src/routing.tsx
+++ src/routing.tsx
@@ -2,21 +2,39 @@ import { createBrowserRouter } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
 import LoginLayout from "./layouts/LoginLayout";
+import MainLayout from "./layouts/MainLayout";
+import LandingPage from "./pages/LandingPage";
 
 
 export const router = createBrowserRouter([
     {
         path: "/",
+        element: <MainLayout />,
+        children: [
+            {
+                index: true,
+                element: <LandingPage />
+            }
+        ]
+    },
+    {
+        path: "/login",
         element: <LoginLayout />,
         children: [
             {
-                path: "/",
+                index: true,
                 element: <LoginPage />
             },
+        ]
+    },
+    {
+        path: "/signup",
+        element: <LoginLayout />,
+        children: [
             {
-                path: "/signup",
+                index: true,
                 element: <SignupPage />
-            }
+            },
         ]
     }
 ])
