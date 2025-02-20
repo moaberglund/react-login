186fcf0611ee1af2b9f9ea955a905da0c04a3ec5
moaberglund <moa.21@hotmail.com>
2025-02-20 13:50:38 +0100

    add ProtectedRoute component for user authentication

---
+++ src/components/auth/ProtectedRoute.tsx
@@ -0,0 +1,26 @@
+import { Navigate } from "react-router-dom";
+import { ReactNode } from "react";
+import { useAuth } from "../../context/AuthContext";
+
+interface ProtectedRouteProps {
+    children: ReactNode
+}
+
+const ProtectedRoute = ({children}: ProtectedRouteProps) => {
+
+    // Get user from context
+    const {user} = useAuth();
+
+    // Redirect to login if user is not logged in
+    if (!user) {
+        return <Navigate to="/login" replace />
+    }
+
+    return (
+        <>
+            {children}
+        </>
+    )
+}
+
+export default ProtectedRoute
