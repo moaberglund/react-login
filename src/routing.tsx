35ff52386f9ec64f2a925da3aaf5e911905a977d
moaberglund <moa.21@hotmail.com>
2025-02-20 11:01:54 +0100

    signup routing

--- src/routing.tsx
+++ src/routing.tsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
 
 
 export const router = createBrowserRouter([
     {
         path: "/",
         element: <LoginPage />
+    },
+    {
+        path: "/signup",
+        element: <SignupPage />
     }
 ])
