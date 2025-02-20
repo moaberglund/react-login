6a8844e2b28ca7c99f4ef67756d9778446901333
moaberglund <moa.21@hotmail.com>
2025-02-20 15:35:04 +0100

    add logout button to LandingPage

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,9 +1,15 @@
+import { useAuth } from "../context/AuthContext"
 
 
 const LandingPage = () => {
+
+    const {logout} = useAuth();
+
     return (
         <div>
             <h1>Landingpage</h1>
+
+            <button onClick={logout}>Log out</button>
         </div>
     )
 }
