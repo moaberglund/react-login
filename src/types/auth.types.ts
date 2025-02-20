f0af6623ab0ec3bca86f563c433f0836d758a115
moaberglund <moa.21@hotmail.com>
2025-02-20 13:13:31 +0100

    interfaces / types

---
+++ src/types/auth.types.ts
@@ -0,0 +1,32 @@
+export interface User {
+    id: string,
+    username: string,
+    password: string,
+    email: string,
+    firstname: string,
+    lastname: string,
+    age: number,
+    created_at: Date
+}
+
+export interface LoginCredentials {
+    username: string,
+    password: string
+}
+
+export interface RegisterCredentials {
+    username: string,
+    password: string
+}
+
+export interface AuthResponse {
+    user: User,
+    token: string
+}
+
+export interface AuthContextType {
+    user: User | null,
+    login: (credentials: LoginCredentials) => Promise<void>,
+    register: (credentials: RegisterCredentials) => Promise<void>,
+    logout: () => void;
+}
