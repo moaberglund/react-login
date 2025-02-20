f681950b9e0ba641d8c00be61c2950975dcf7b85
moaberglund <moa.21@hotmail.com>
2025-02-20 11:04:15 +0100

    include navigation

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,8 +1,10 @@
 import LoginForm from "../components/LoginForm"
+import LoginNavigation from "../components/LoginNavigation"
 
 const LoginPage = () => {
   return (
     <div>
+        <LoginNavigation />
         <h1>Login</h1>
         <LoginForm />
     </div>
--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -1,8 +1,10 @@
+import LoginNavigation from "../components/LoginNavigation"
 
 
 const SignupPage = () => {
   return (
     <div>
+        <LoginNavigation />
         <h1>Sign up</h1>
         
     </div>
