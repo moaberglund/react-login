2e995b41b8d06a287058ae629209ab8e3f25b9b9
moaberglund <moa.21@hotmail.com>
2025-02-20 14:03:45 +0100

    wrap RouterProvider with AuthProvider for authentication context

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import './styles/main.scss'
 import { router } from './routing.tsx'
+import { AuthProvider } from './context/AuthContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router}></RouterProvider>
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </StrictMode>,
 )
