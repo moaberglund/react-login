6f93a3add2ab2f2f5edf4da18a6f08ef8125fd5b
moaberglund <moa.21@hotmail.com>
2025-02-20 14:37:20 +0100

    implement SignupPage with username and password registration, including error handling and navigation

--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -1,11 +1,56 @@
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+import { useState } from "react";
 
 
 const SignupPage = () => {
+
+  // States
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const { register } = useAuth();
+  const navigate = useNavigate();
+
+  // Functions
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setError('');
+
+    try {
+      await register({ username, password });
+      navigate('/login');
+    } catch (err) {
+      setError("Failed to register. Control username and password");
+    }
+  }
+
   return (
     <div>
 
-        <h1>Sign up</h1>
-        
+      <h1>Sign up</h1>
+
+      <div className="auth-form">
+
+        <form onSubmit={handleSubmit}>
+          {error && <p className="error">{error}</p>}
+
+          <div className="form-control">
+            <label htmlFor="username">Username</label>
+            <input type="username" id="username" name="username" required value={username} onChange={(e) => setUsername(e.target.value)} />
+          </div>
+
+          <div className="form-control">
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" name="password" required value={password} onChange={(e) => setPassword(e.target.value)} />
+          </div>
+
+          <button className="btn submit-btn" type="submit">Sign up</button>
+
+        </form>
+      </div>
+
     </div>
   )
 }
