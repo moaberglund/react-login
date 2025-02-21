634f02965b91b0b9ab707e84dbcdbdc844aaed2a
moaberglund <moa.21@hotmail.com>
2025-02-21 15:03:11 +0100

    test prompt for username on landingpage

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -3,11 +3,12 @@ import { useAuth } from "../context/AuthContext"
 
 const LandingPage = () => {
 
-    const {logout} = useAuth();
+    const {user, logout} = useAuth();
 
     return (
         <div>
             <h1>Landingpage</h1>
+            <p>Welcome {user ? user.username : ""}</p>
 
             <button onClick={logout}>Log out</button>
         </div>
