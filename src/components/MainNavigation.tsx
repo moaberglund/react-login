abb3b14b83bdb65a7812f700c894603715640f23
moaberglund <moa.21@hotmail.com>
2025-02-28 12:23:03 +0100

    refactor MainNavigation component to use header element for improved semantics

--- src/components/MainNavigation.tsx
+++ src/components/MainNavigation.tsx
@@ -3,13 +3,16 @@ import { NavLink } from "react-router-dom"
 
 const MainNavigation = () => {
   return (
-    <nav className="main-navigation">
+    <header>
+      <nav className="main-navigation">
         <ul>
-            <li><NavLink to="/">Home</NavLink></li>
-            <li><NavLink to="/notes">Notes</NavLink></li>            
-            
+          <li><NavLink to="/">Home</NavLink></li>
+          <li><NavLink to="/notes">Notes</NavLink></li>
+
         </ul>
-    </nav>
+      </nav>
+      
+    </header>
   )
 }
 
