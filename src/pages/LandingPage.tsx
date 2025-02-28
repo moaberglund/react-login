50451e45895fc99ef73df906fb4d7766b38c6429
moaberglund <moa.21@hotmail.com>
2025-02-28 14:29:37 +0100

    remove logout button from LandingPage component

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -3,14 +3,12 @@ import { useAuth } from "../context/AuthContext"
 
 const LandingPage = () => {
 
-    const {user, logout} = useAuth();
+    const {user} = useAuth();
 
     return (
         <div>
             <h1>Landingpage</h1>
             <p>Welcome {user ? user.username : ""}</p>
-
-            <button onClick={logout}>Log out</button>
         </div>
     )
 }
