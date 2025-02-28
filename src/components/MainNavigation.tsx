b5aaf0999ecd2fe80eac83d1377b43e9788c81b3
moaberglund <moa.21@hotmail.com>
2025-02-28 14:20:40 +0100

    add logout functionality to MainNavigation component

--- src/components/MainNavigation.tsx
+++ src/components/MainNavigation.tsx
@@ -1,17 +1,21 @@
 import { NavLink } from "react-router-dom"
+import { useAuth } from "../context/AuthContext";
+import { FaPowerOff  } from "react-icons/fa6";
 
 
 const MainNavigation = () => {
+
+  const { logout } = useAuth();
+
   return (
     <header>
       <nav className="main-navigation">
         <ul>
           <li><NavLink to="/">Home</NavLink></li>
           <li><NavLink to="/notes">Notes</NavLink></li>
-
+          <li className="logout" onClick={logout}><FaPowerOff /></li>
         </ul>
       </nav>
-      
     </header>
   )
 }
