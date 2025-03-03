5d4a6d20d7e579dc9d0e53bd7ee5a92fe17acb8c
moaberglund <moa.21@hotmail.com>
2025-03-03 13:11:10 +0100

    refactor EditNotePage to integrate Yup validation and structured error handling

--- src/pages/notes/edit.tsx
+++ src/pages/notes/edit.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import * as Yup from "yup";
+import ErrorsData from "../../interfaces/ErrorsData";
 
 const EditNotePage = () => {
   const { id } = useParams<{ id: string }>();
@@ -7,6 +9,13 @@ const EditNotePage = () => {
   const [note, setNote] = useState({ title: "", text: "" });
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
+  const [errors, setErrors] = useState<ErrorsData>({});
+
+  // Yup
+  const validationSchema = Yup.object({
+    title: Yup.string().required('Please enter a title.').min(3, "The title must be at least 3 character."),
+    text: Yup.string().required("Please enter a text.").min(10, "A good text should be a least 10 characters long, let's try again.").max(200, "A description longer than 200 characters is a bit much, don't you think? Let's shorten it a bit.")
+  });
 
   useEffect(() => {
     if (id) {
@@ -39,11 +48,17 @@ const EditNotePage = () => {
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
+    setLoading(true);
+    setErrors({});
 
     const token = localStorage.getItem("token");
 
     try {
-      setLoading(true);
+
+      // Validate
+      await validationSchema.validate(note, { abortEarly: false });
+
+      // Send data to the API
       const resp = await fetch(`https://protectednotes-api.onrender.com/note/${id}`, {
         method: "PUT",
         headers: {
@@ -58,9 +73,20 @@ const EditNotePage = () => {
       }
 
       navigate(`/notes/${id}`);
+
     } catch (err) {
-      console.error("Error updating note:", err);
-      setError("Failed to update note.");
+
+      if (err instanceof Yup.ValidationError) {
+        const validationErrors: ErrorsData = {};
+
+        err.inner.forEach(error => {
+          const prop = error.path as keyof ErrorsData;
+          validationErrors[prop] = error.message;
+        });
+
+        setErrors(validationErrors);
+        return;
+      }
     } finally {
       setLoading(false);
     }
@@ -75,8 +101,10 @@ const EditNotePage = () => {
       <form onSubmit={handleSubmit}>
 
         <input type="text" name="title" value={note.title} onChange={handleChange} required />
+        {errors.title && <span className="error-message">{errors.title}</span>}
 
         <textarea name="text" value={note.text} onChange={handleChange} required />
+        {errors.text && <span className="error-message">{errors.text}</span>}
 
         <button className="btn-create" style={{ marginTop: '2em' }} type="submit" disabled={loading}>
           {loading ? "Saving..." : "Save changes"}
