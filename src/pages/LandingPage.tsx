6b08e87e0ecdc117ccd1b4dd549053b2aac63c9c
moaberglund <moa.21@hotmail.com>
2025-03-03 11:07:56 +0100

    add informational section to LandingPage about posting notes

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -9,6 +9,11 @@ const LandingPage = () => {
         <div>
             <h1>Landingpage</h1>
             <p>Welcome {user ? user.username : ""}</p>
+
+            <section style={{ marginTop: '2em' }}>
+                <h2>Note to Everyone</h2>
+                <p>On this platform you can post Notes that you think Everyone should know about.</p>
+            </section>
         </div>
     )
 }
