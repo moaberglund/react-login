4b58d8875bef9b1fab153d53bc6586275137ec96
moaberglund <moa.21@hotmail.com>
2025-03-03 12:26:02 +0100

    refactor CreateNotePage to use Yup for validation and manage note data with a single state object

--- src/pages/notes/create.tsx
+++ src/pages/notes/create.tsx
@@ -1,38 +1,76 @@
 import { useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 import { FaChevronLeft } from "react-icons/fa6";
+import { NoteData } from "../../interfaces/NoteData";
+import ErrorsData from "../../interfaces/ErrorsData";
+import * as Yup from "yup";
 
 const CreateNotePage = () => {
+
+  const [noteData, setNoteData] = useState<NoteData>({
+    _id: "",
+    title: "",
+    text: "",
+  });
+
   const navigate = useNavigate();
-  const [title, setTitle] = useState("");
-  const [text, setText] = useState("");
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState("");
+
+  // Error State
+  const [errors, setErrors] = useState<ErrorsData>({});
+
+  // Yup
+  const validationSchema = Yup.object({
+    title: Yup.string().required('Please enter a title.').min(3, "The title must be at least 3 character."),
+    text: Yup.string().required("Please enter a text.").min(10, "A good text should be a least 10 characters long, let's try again.").max(200, "A description longer than 200 characters is a bit much, don't you think? Let's shorten it a bit.")
+  });
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     setLoading(true);
-    setError("");
 
     const token = localStorage.getItem("token");
 
     try {
+
+      // Validate
+      await validationSchema.validate(noteData, { abortEarly: false });
+
+      // Send data to the API
       const response = await fetch("https://protectednotes-api.onrender.com/note", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
           "Authorization": `Bearer ${token}`,
         },
-        body: JSON.stringify({ title, text }),
+        body: JSON.stringify(noteData),
       });
 
       if (!response.ok) {
         throw new Error("Failed to create note");
       }
 
+      setErrors({});
+      setNoteData({
+        _id: '',
+        title: '',
+        text: ''
+      });
+
       navigate("/notes"); // Skicka tillbaka användaren till listan efter skapandet
-    } catch (err) {
-      setError("Failed to create note. Please try again.");
+
+    } catch (errors) {
+      const validationErrors: ErrorsData = {};
+
+      if (errors instanceof Yup.ValidationError) {
+        errors.inner.forEach(error => {
+          const prop = error.path as keyof ErrorsData;
+
+          validationErrors[prop] = error.message;
+        })
+        setErrors(validationErrors);
+        return;
+      }
     } finally {
       setLoading(false);
     }
@@ -41,24 +79,24 @@ const CreateNotePage = () => {
   return (
     <div>
       <h1>Create a New Note</h1>
-      {error && <p style={{ color: "red" }}>{error}</p>}
       <form onSubmit={handleSubmit}>
-
-
         <input
           aria-label="titleinput"
           placeholder="Title"
           type="text"
-          value={title} onChange={(e) => setTitle(e.target.value)}
+          value={noteData.title} 
+          onChange={(e) => setNoteData({ ...noteData, title: e.target.value })}
           required />
+        {errors.title && <span className="error-message">{errors.title}</span>}
 
 
         <textarea
           aria-label="textinput"
           placeholder="Write your note here..."
-          value={text}
-          onChange={(e) => setText(e.target.value)}
+          value={noteData.text}
+          onChange={(e) => setNoteData({ ...noteData, text: e.target.value })}
           required />
+        {errors.text && <span className="error-message">{errors.text}</span>}
 
         <button className="btn-create" style={{ marginTop: '2em' }} type="submit" disabled={loading}>
           {loading ? "Creating..." : "Create Note"}
